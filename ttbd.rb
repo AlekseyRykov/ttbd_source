34dc86df981413c33b095454457f420604d18193
Aleksey Rykov <a.rykov8923@gmail.com>
2018-10-11 14:24:15 +1100

    Feature
    add animation of loading
    add add_task and show_task methods
    add interaction with tasks (add, show) via command-line

--- ttbd.rb
+++ ttbd.rb
@@ -5,6 +5,14 @@ def tdate
   puts "\rToday is #{Date.today}"
 end
 
+def loading
+  5.times do
+    print "."
+    sleep rand(0.01..0.3)
+  end
+  print "\r"
+end
+
 def add_category
   print 'Enter category name: '
   category_name = gets.strip
@@ -30,9 +38,45 @@ def delete_category
   end
 end
 
-@tasks = []
+def add_task
+  puts 'In which category add task? Choose number: '
+  show_category
+  print 'Enter: '
+  category_index = gets.strip.to_i - 1
+  print 'New task: '
+  new_task = gets.chomp.capitalize
+  print 'Date (00-00-0000): '
+  date = gets.strip
+  print 'Time (00:00): '
+  time = gets.strip
+  @tasks[category_index] << { 'task' => new_task, 'date' => date, 'time' => time }
+end
 
+def show_task
+  if @tasks.empty?
+    puts 'There is no tasks.'
+  elsif
+    @tasks.each_with_index do |name, index|
+      array_length = name.length
+      if name[1..array_length].empty?
+        puts 'There is no tasks.'
+      else
+        task_number = 1
+        puts "#{name[0]}"
+        until task_number == array_length
+          puts "#{task_number}. #{name[task_number]['task']} // #{name[task_number]['date']} // #{name[task_number]['time']}"
+          task_number += 1
+        end
+      end
+    end
+  end
+end
+
+puts "\n\"THINGS TO BE DONE\"\n"
+loading
 tdate
+
+@tasks = []
 
 while true
 
@@ -51,6 +95,10 @@ while true
     show_category
   elsif command == '3'
     delete_category
+  elsif command == 'A'
+    add_task
+  elsif command == 'S'
+    show_task
   elsif command == "T"
     tdate
   else
